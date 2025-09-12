9f1ae466a65073ecbaa3809bbc9e80e11398d1cb
KetanDav <davketan029@gmail.com>
2025-09-12 18:11:51 +0530

    sovled error that was problemetic for loading now its working fine

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,16 +1,15 @@
-import { StrictMode, useEffect, useState, useContext } from "react";
+import { StrictMode, useEffect, useState } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./pages/App";
 import About from "./pages/About";
 import Analysis from "./pages/Analysis";
 import MapPanel from "./pages/MapPanel";
-import { IpProvider, IpContext } from "./components/IpContext";
+import { IpProvider } from "./components/IpContext";
 import "./index.css";
 
 function Main() {
-  const { latestIp, setLatestIp } = useContext(IpContext);
-  console.log("Main IP:", latestIp); // Debug log
+
 
   return (
     <StrictMode>
